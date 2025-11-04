39d699b91dc51e5e1a7f159b4a002505d3e30be5
devzeus <hieu01bdvn@gmail.com>
2025-11-04 07:43:10 +0700

    first commit

---
+++ vibecoders_backend/src/test/java/hcmute/hackathon/vibecoders/VibecodersApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.hackathon.vibecoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibecodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
